a6713014ecc6adc301f6d63419c7a01775c17cd7
Aadil Hussain <aadil_hussain77@yahoo.com>
2017-07-12 00:54:30 +0530

    initial project version

---
+++ MYATM2017/Controllers/CheckingAccountController.cs
@@ -0,0 +1,97 @@
+﻿using MYATM2017.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MYATM2017.Controllers
+{
+    public class CheckingAccountController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: CheckingAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CheckingAccount/Details/5
+        public ActionResult Details(int checkingAccountId)
+        {
+            var checkingAccount = new CheckingAccount();
+            var checkingAccountList = db.CheckingAccounts.Where(C => C.Id == checkingAccountId);
+            foreach (var item in checkingAccountList)
+            {
+                checkingAccount = item;
+            }
+            return View(checkingAccount);
+        }
+
+        // GET: CheckingAccount/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CheckingAccount/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CheckingAccount/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CheckingAccount/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CheckingAccount/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CheckingAccount/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MYATM2017/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MYATM2017.Models;
+using Microsoft.AspNet.Identity;
+
+namespace MYATM2017.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        [MyLoggingFilter]
+        [Authorize]
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var checkingAccountId = db.CheckingAccounts.Where(c=>c.ApplicationUserId==userId).First().Id;
+            ViewBag.CheckingAccountId = checkingAccountId;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.TheMessage = "Having some trouble? Send us a message!";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(string  message)
+        {
+            ViewBag.TheMessage = "Thanks, We got Your Message !!!!";
+
+            return View();
+        }
+        public ActionResult Serial(string letterCase) {
+            var serial = "ASPNETMVC17 ";
+            if (letterCase=="lower")
+            {
+                return Content(serial.ToLower());
+            }
+            else
+            {
+                return Content(serial.ToUpper());
+                //return Json(new { name="Serial", value=serial},JsonRequestBehavior.AllowGet);
+                //return RedirectToAction("Index");
+            }
+            
+        }
+    }
+}
